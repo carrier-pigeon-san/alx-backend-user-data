7dca4c66ab485de56506f054ae61770b18c0708d
Sandy Obare <obaresandy@gmail.com>
2024-11-13 02:45:56 +0300

    api/v1/auth/basic_auth.py user_object_from_credentials method implementation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """BasicAuth class to manage the API authentication"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -30,3 +31,30 @@ class BasicAuth(Auth):
                 decode('utf-8')
         except Exception:
             return None
+        
+    def extract_user_credentials(self,
+                                decoded_base64_authorization_header: str
+                                ) -> (str, str):
+        """Returns the user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None or \
+                type(decoded_base64_authorization_header) is not str or \
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':', 1))
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                        ) -> TypeVar('User'):
+            """Returns the User instance based on his email and password"""
+            if user_email is None or type(user_email) is not str or \
+                    user_pwd is None or type(user_pwd) is not str:
+                return None
+            try:
+                users = User.search({'email': user_email})
+                if len(users) == 0:
+                    return None
+                user = users[0]
+                if not user.is_valid_password(user_pwd):
+                    return None
+                return user
+            except Exception:
+                return None
