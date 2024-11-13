b2b6d57d6f1204ad5b105cfc236d660e245ba48f
Sandy Obare <obaresandy@gmail.com>
2024-11-13 03:05:09 +0300

    api/v1/auth/basic_auth.py BasicAuth.current_user method implementatiton

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,3 +59,12 @@ class BasicAuth(Auth):
             return user
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        user_credentials = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user_credentials[0],
+                                                 user_credentials[1])
