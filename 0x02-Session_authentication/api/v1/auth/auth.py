16174a8096e5229042019299c2c1634a695301b7
Sandy Obare <obaresandy@gmail.com>
2024-11-14 14:00:09 +0300

    0x02-Session_authentication/ project structure setup

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Auth class module.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth method.
+        """
+        if path is not None and excluded_paths is not None \
+                and len(excluded_paths) > 0:
+            for p in excluded_paths:
+                if "".join(p.split("/")) == "".join(path.split("/")):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header method.
+        """
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user method.
+        """
+        return None
