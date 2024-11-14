cfd3686c57c9acb6c83281b8908303f13f0f2048
Sandy Obare <obaresandy@gmail.com>
2024-11-14 16:04:29 +0300

    api/v1/views/users.py '/users/me' route implementation, '/users/<user_id>' route modification

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,6 +27,10 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == "me" and request.current_user is None:
+        abort(404)
+    if user_id == "me" and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -120,3 +124,12 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def get_user_me() -> str:
+    """ GET /api/v1/users/me
+    Return:
+      - User object JSON represented
+    """
+    return jsonify(request.current_user.to_json())
