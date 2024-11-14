f459fd1f8b34adae0274d6565a45b2662079d2cc
Sandy Obare <obaresandy@gmail.com>
2024-11-15 00:28:08 +0300

    api/v1/views/session_auth.py login route logic update

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -27,7 +28,7 @@ def login() -> str:
         return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
-    SESSION_NAME = 'session_id'
+    session_name = os.getenv("SESSION_NAME")
     response = jsonify(user.to_json())
-    response.set_cookie(SESSION_NAME, session_id)
+    response.set_cookie(session_name, session_id)
     return response
