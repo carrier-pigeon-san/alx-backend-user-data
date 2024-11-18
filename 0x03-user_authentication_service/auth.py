8fbea520087bbd3e15ec6e0698eb976b11072655
Sandy Obare <obaresandy@gmail.com>
2024-11-19 01:31:33 +0300

    auth.py initial commit: _hash_password method implementation, Auth class implementation, Auth.register_user method implementation

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" User authentication module
+"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is encoded in bytes
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
